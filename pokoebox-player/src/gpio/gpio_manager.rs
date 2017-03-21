c4634204dac9e30cb6d7b118baece4ea9d5bf951
timvisee <timvisee@gmail.com>
2017-03-21 14:45:15 +0100

    Add CuPi instance to GPIO manager

--- pokoebox-player/src/gpio/gpio_manager.rs
+++ pokoebox-player/src/gpio/gpio_manager.rs
@@ -1,11 +1,31 @@
 #![cfg(feature = "rpi")]
 
+use super::cupi::CuPi;
+
+use error::Error;
+
 /// GPIO manager.
-pub struct GpioManager;
+pub struct GpioManager {
+    cupi: CuPi
+}
 
 impl GpioManager {
     /// Constructor.
-    pub fn new() -> Self {
-        GpioManager {}
+    pub fn new() -> Result<Self, Error> {
+        // Initialize CuPi
+        let cupi = CuPi::new();
+        if cupi.is_err() {
+            return Err(Error::new("Failed to initialize CuPi for GPIO."));
+        }
+
+        // Construct and return
+        GpioManager {
+            cupi: cupi
+        }
+    }
+
+    /// Get the CuPi instance.
+    pub fn cupi(&self) -> &CuPi {
+        &self.cupi
     }
 }
