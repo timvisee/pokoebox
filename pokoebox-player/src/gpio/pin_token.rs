2a353c67aca68ea8da59d7b242f1e27a3aa2e134
timvisee <timvisee@gmail.com>
2017-03-30 18:31:13 +0200

    Implement display trait for pin token

--- pokoebox-player/src/gpio/pin_token.rs
+++ pokoebox-player/src/gpio/pin_token.rs
@@ -1,14 +1,23 @@
 #![cfg(feature = "rpi")]
 
+use std::fmt::{Display, Formatter, Result};
+
 /// Token of a pin.
 /// Pin tokens are used to identify a pin. Each pin that is created gets it's own unique token.
 /// Pins can then be requested from the GPIO manager by their token.
-#[derive(PartialEq, Eq, Copy, Clone, Hash)]
+#[derive(PartialEq, Eq, Copy, Clone, Hash, Debug)]
 pub struct PinToken(usize);
 
 impl PinToken {
     /// Construct a new pin token with the given token value.
     pub fn new(token: usize) -> PinToken {
         PinToken(token)
+    }
+}
+
+/// Make a pin token displayable.
+impl Display for PinToken {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.0)
     }
 }
