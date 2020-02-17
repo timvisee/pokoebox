f4a851113da2249675be65e67b511c57981300ee
timvisee <tim@visee.me>
2020-02-17 17:03:04 +0100

    Handle serial input commands as well in LED controller firmware, add help

--- firmware-led-controller/firmware-led-controller.ino
+++ firmware-led-controller/firmware-led-controller.ino
@@ -4,8 +4,9 @@ const int I2C_ADDR = 8;
 const int LED_PINS [] = { 8, 9, 10, 11, 12 };
 const int LED_COUNT = sizeof(LED_PINS) / sizeof(int);
 
-// i2c buffer
-String buff = "";
+// Input buffers
+String i2c_buff = "";
+String serial_buff = "";
 
 void setup() {
     // Configure pins
@@ -24,23 +25,38 @@ void setup() {
 }
 
 void loop() {
+    // Sink serial input to buffer
+    while(Serial.available()) {
+        int b = Serial.read();
+        if(b == 0)
+            continue;
+        serial_buff += (char) b;
+    }
+
+    // Handle serial commands
+    int i;
+    while((i = serial_buff.indexOf('\n')) > -1) {
+        onCommand(serial_buff.substring(0, i));
+        serial_buff.remove(0, i + 1);
+    }
+
     delay(100);
 }
 
 void onReceive(int count) {
-    // Fill buffer
+    // Sink i2c input to buffer
     while(Wire.available()) {
         int b = Wire.read();
         if(b == 0)
             continue;
-        buff += (char) b;
+        i2c_buff += (char) b;
     }
 
-    // Handle each command
+    // Handle i2c commands
     int i;
-    while((i = buff.indexOf('\n')) > -1) {
-        onCommand(buff.substring(0, i));
-        buff.remove(0, i + 1);
+    while((i = i2c_buff.indexOf('\n')) > -1) {
+        onCommand(i2c_buff.substring(0, i));
+        i2c_buff.remove(0, i + 1);
     }
 }
 
@@ -49,10 +65,15 @@ void onCommand(String cmd) {
     Serial.println("CMD: '" + cmd + "'");
 
     // Handle commands
-    if(cmd.startsWith("led "))
+    if(cmd == "help") {
+        Serial.println("HELP:");
+        Serial.println("- help: show help");
+        Serial.println("- led reset: reset all LEDs");
+        Serial.println("- led <INDEX> <LEVEL>: set LED level");
+    } else if(cmd.startsWith("led "))
         onLedCommand(cmd.substring(4));
     else
-        Serial.println("CMD '" + cmd + "' is unknown");
+        Serial.println("CMD '" + cmd + "' is unknown, use 'help'");
 }
 
 void onLedCommand(String cmd) {
