a806d83d542b1439d9bb2d333a8ab7b8bd76f31a
timvisee <tim@visee.me>
2020-03-13 14:29:15 +0100

    Show source items in player list

--- pokoebox-player/src/ui/gtk/pages/player.rs
+++ pokoebox-player/src/ui/gtk/pages/player.rs
@@ -1,8 +1,9 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex, MutexGuard};
 
 use glib::clone;
 use gtk::prelude::*;
 use pokoebox_media::mpris::Cmd;
+use pokoebox_media::player::{Handle, State};
 
 use crate::app::Core;
 use crate::pages::PageType;
@@ -34,36 +35,27 @@ impl Player {
 
         page
     }
-}
 
-impl Page for Player {
-    fn page_type(&self) -> PageType {
-        PAGE_TYPE
-    }
-
-    fn page_name(&self) -> &'static str {
-        &PAGE_NAME
-    }
-
-    fn build_page(&self, core: Arc<Core>) {
-        // Configure the page
-        self.container.set_halign(gtk::Align::Center);
-        self.container.set_valign(gtk::Align::Center);
-
-        // Main container
-        let container = gtk::Box::new(gtk::Orientation::Vertical, BUTTON_SPACING as i32);
-        self.container.add(&container);
+    fn build_controls_container(core: &Arc<Core>) -> gtk::Box {
+        // Controls container
+        let controls = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(BUTTON_SPACING as i32)
+            .expand(true)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .build();
 
         let player_label = gtk::Label::new(Some("Sources: ?"));
-        container.add(&player_label);
+        controls.add(&player_label);
 
         // Add source label
         let source_label = gtk::Label::new(Some("Source: ?"));
-        container.add(&source_label);
+        controls.add(&source_label);
 
         // Add track info label
         let track_info = gtk::Label::new(Some("Track: ?"));
-        container.add(&track_info);
+        controls.add(&track_info);
 
         // Create a button grid
         let btns = gtk::Grid::new();
@@ -71,7 +63,7 @@ impl Page for Player {
         btns.set_column_spacing(BUTTON_SPACING);
         btns.set_row_homogeneous(true);
         btns.set_column_homogeneous(true);
-        container.add(&btns);
+        controls.add(&btns);
 
         let btn_prev = gtk::Button::new_from_icon_name(
             Some("media-skip-backward"),
@@ -192,9 +184,195 @@ impl Page for Player {
                 err
             ),
         }
+
+        controls
+    }
+
+    fn build_sources_container(core: &Arc<Core>) -> Arc<SourceList> {
+        // Create source list
+        let source_list = Arc::new(SourceList::build());
+
+        // Handle player events
+        let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT_IDLE);
+        core.player.events.register_callback(move |event| {
+            if let Err(err) = tx.send(event) {
+                error!("Failed to send player event to Glib: {:?}", err);
+            }
+        });
+        rx.attach(
+            None,
+            // TODO: use @weak?
+            clone!(@strong source_list => @default-return glib::Continue(false), move |event| {
+                use pokoebox_media::player::player::Event;
+                use pokoebox_media::player::source::Event as SourceEvent;
+
+                match event {
+                    Event::Source(event) => match event {
+                        SourceEvent::States(states) => source_list.update_sources(states),
+                        SourceEvent::Add(_, _) | SourceEvent::Remove(_) => {}
+                    },
+                }
+
+                glib::Continue(true)
+            }),
+        );
+
+        // Emit last source states
+        match core.player.sources.lock() {
+            Ok(sources) => sources.emit_states(),
+            Err(err) => error!(
+                "Failed to request player to emit states event, ignoring: {:?}",
+                err
+            ),
+        }
+
+        source_list
+    }
+}
+
+impl Page for Player {
+    fn page_type(&self) -> PageType {
+        PAGE_TYPE
+    }
+
+    fn page_name(&self) -> &'static str {
+        &PAGE_NAME
+    }
+
+    fn build_page(&self, core: Arc<Core>) {
+        // Main container.
+        let container = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Horizontal)
+            .spacing(BUTTON_SPACING as i32)
+            .expand(true)
+            .build();
+        self.container.add(&container);
+
+        // Build containers
+        container.add(&Self::build_controls_container(&core));
+        container.add(&Self::build_sources_container(&core).container);
     }
 
     fn gtk_widget(&self) -> &gtk::Grid {
         &self.container
+    }
+}
+
+struct SourceList {
+    /// The container.
+    pub container: gtk::ScrolledWindow,
+
+    /// The list box.
+    list: gtk::ListBox,
+
+    /// List of sources
+    sources: Mutex<Vec<Source>>,
+}
+
+impl SourceList {
+    fn build() -> Self {
+        // Base container, scrollable
+        let container = gtk::ScrolledWindowBuilder::new()
+            .expand(true)
+            .shadow_type(gtk::ShadowType::EtchedIn)
+            .build();
+
+        // Source list
+        let list = gtk::ListBoxBuilder::new()
+            // .spacing(4)
+            // .orientation(gtk::Orientation::Vertical)
+            .expand(true)
+            // .halign(gtk::Align::Start)
+            // .valign(gtk::Align::Start)
+            .border_width(40)
+            .build();
+        container.add(&list);
+
+        Self {
+            container,
+            list,
+            sources: Mutex::new(Vec::new()),
+        }
+    }
+
+    pub fn update_sources(&self, states: Vec<(Handle, State)>) {
+        // Lock sources list
+        let mut sources = self.sources.lock().expect("Failed to lock sources list");
+
+        // Add new sources, remove old sources
+        // TODO: update changed sources
+        states.iter().for_each(|(handle, _state)| {
+            if !sources.iter().any(|s| &s.handle == handle) {
+                self.add_source(&mut sources, *handle);
+            }
+        });
+
+        sources
+            .iter()
+            .filter(|source| !states.iter().any(|(handle, _)| &source.handle == handle))
+            .map(|source| source.handle)
+            .collect::<Vec<Handle>>()
+            .iter()
+            .for_each(|h| {
+                if !states.iter().any(|(handle, _)| h == handle) {
+                    self.remove_source(&mut sources, *h);
+                }
+            });
+
+        // Update view
+        self.list.show_all();
+    }
+
+    fn add_source<'a>(&self, sources: &mut MutexGuard<'a, Vec<Source>>, handle: Handle) {
+        let source = Source::build(handle);
+        self.list.insert(&source.row, -1);
+        sources.push(source);
+    }
+
+    fn remove_source<'a>(&self, sources: &mut MutexGuard<'a, Vec<Source>>, handle: Handle) {
+        let source = match sources.iter().find(|s| s.handle == handle) {
+            Some(source) => source,
+            None => return,
+        };
+
+        self.list.remove(&source.container);
+    }
+}
+
+struct Source {
+    pub row: gtk::ListBoxRow,
+    pub container: gtk::Box,
+    pub handle: Handle,
+}
+
+impl Source {
+    fn build(handle: Handle) -> Self {
+        // Build container
+        let container = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Vertical)
+            // .margin(4)
+            .hexpand(true)
+            .halign(gtk::Align::Start)
+            .spacing(4)
+            .margin(4)
+            .build();
+
+        // Add labels
+        let source_lbl = gtk::LabelBuilder::new()
+            .label(&format!("<b>Source: {:?}</b>", handle))
+            .use_markup(true)
+            .build();
+        container.add(&source_lbl);
+
+        let track_lbl = gtk::Label::new(Some("Track"));
+        container.add(&track_lbl);
+
+        let row = gtk::ListBoxRowBuilder::new().child(&container).build();
+
+        Self {
+            row,
+            container,
+            handle,
+        }
     }
 }
